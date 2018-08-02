4848be20a99de5dfbcdbf84be3bcb5e2c421e3dc
tbindseil <tbindseil@yahoo.com>
2018-08-01 19:25:43 -0500

    implement pop up color and thickness

--- app/src/main/java/com/tj/drawwithfriends2/ChooseOrCreateActivity.java
+++ app/src/main/java/com/tj/drawwithfriends2/ChooseOrCreateActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
--- app/src/main/java/com/tj/drawwithfriends2/ProjectActivity.java
+++ app/src/main/java/com/tj/drawwithfriends2/ProjectActivity.java
@@ -1,12 +1,30 @@
 package com.tj.drawwithfriends2;
 
+import android.content.Context;
+import android.graphics.Rect;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
 
 public class ProjectActivity extends AppCompatActivity {
     private ProjectFile currProject;
+
+    private ConstraintLayout normalLayout;
+
+    private SeekBar thicknessBar;
+    private int thickness;
+
+    private LinearLayout colorSeekBars;
+    private int red, green, blue;
+
+    private View currFocus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +45,78 @@ public class ProjectActivity extends AppCompatActivity {
         // Get a support ActionBar corresponding to this toolbar
         ActionBar ab = getSupportActionBar();
 
+        normalLayout = findViewById(R.id.projectConstraintLayout);
+        currFocus = normalLayout;
+
         // TODO force save on navigation away from page
         ab.setDisplayHomeAsUpEnabled(true);
+
+        thicknessBar = findViewById(R.id.thicknessSeekBar);
+        thicknessBar.setOnSeekBarChangeListener(new SeekBarInterface() {
+            @Override
+            public void onProgressChanged(android.widget.SeekBar seekBar, int progress, boolean fromUser) {
+                thickness = progress;
+            }
+        });
+
+        colorSeekBars = findViewById(R.id.colorLayout);
+
+        SeekBar s = findViewById(R.id.redSeekBar);
+        s.setOnSeekBarChangeListener(new SeekBarInterface() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                red = progress;
+            }
+        });
+
+        s = findViewById(R.id.greenSeekBar);
+        s.setOnSeekBarChangeListener(new SeekBarInterface() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                green = progress;
+            }
+        });
+
+        s = findViewById(R.id.blueSeekBar);
+        s.setOnSeekBarChangeListener(new SeekBarInterface() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                blue = progress;
+            }
+        });
+    }
+
+    public void handleColorClick(View view) {
+        colorSeekBars.setVisibility(View.VISIBLE);
+
+        currFocus = colorSeekBars;
+    }
+
+    public void handleThicknessClick(View view) {
+        thicknessBar.setVisibility(View.VISIBLE);
+
+        currFocus = thicknessBar;
+    }
+
+    private abstract class SeekBarInterface implements SeekBar.OnSeekBarChangeListener {
+        @Override
+        public abstract void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser);
+        public void onStartTrackingTouch(SeekBar seekBar) {}
+        public void onStopTrackingTouch(SeekBar seekBar) {}
+    }
+
+    // this assumes normalLayout is whole screen i think
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        Rect viewRect = new Rect();
+        currFocus.getGlobalVisibleRect(viewRect);
+        if (viewRect.contains((int)ev.getRawX(), (int)ev.getRawY())) {
+            return super.dispatchTouchEvent(ev);
+        }
+
+        currFocus.setVisibility(View.INVISIBLE);
+        currFocus = normalLayout;
+        currFocus.setVisibility(View.VISIBLE);
+        return true;
     }
 }
