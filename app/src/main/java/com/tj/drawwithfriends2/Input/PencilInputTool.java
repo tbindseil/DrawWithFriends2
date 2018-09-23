f4870462dfbaf661f7ff74da31a6966a747212d2
tbindseil <tbindseil@yahoo.com>
2018-09-23 18:41:12 -0500

    implement bresenham algorithm

--- app/src/main/java/com/tj/drawwithfriends2/Input/PencilInputTool.java
+++ app/src/main/java/com/tj/drawwithfriends2/Input/PencilInputTool.java
@@ -15,6 +15,7 @@ import com.tj.drawwithfriends2.InputTransporter;
 
 @TargetApi(23)
 public class PencilInputTool implements InputTool {
+    Point lastTouch;
     int color;
     int thickness;
 
@@ -37,15 +38,101 @@ public class PencilInputTool implements InputTool {
 
     @Override
     public void handleTouch(MotionEvent event) {
-        int filteredX = filterX((int)event.getX());
-        int filteredY = filterY((int)event.getY());
-
-        if (filteredX >= 0 && filteredX < width && filteredY >= 0 && filteredY < height) {
-            InputTransporter.getInstance().addPoint(filteredX, filteredY, color);
+        if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
+            lastTouch = null;
         }
+
+        int filteredX = filterX((int) event.getX());
+        int filteredY = filterY((int) event.getY());
+        Point currTouch = new Point(filteredX, filteredY);
+
+        if (filteredX < 0) {
+            filteredX = 0;
+        }
+        if (filteredX >= width) {
+            filteredX = width;
+        }
+        if (filteredY < 0) {
+            filteredY = 0;
+        }
+        if (filteredY >= height) {
+            filteredY = height;
+        }
+
+        if (lastTouch == null) {
+            InputTransporter.getInstance().addPoint(filteredX, filteredY, color);
+        } else {
+            plotLine(currTouch);
+        }
+
+        lastTouch = currTouch;
 
         if (event.getActionMasked() == MotionEvent.ACTION_UP) {
             InputTransporter.getInstance().finishInput();
+        }
+    }
+
+    // thanks wikipedia and Jack Bresenham
+
+    private void plotLineLow(int x0, int y0, int x1, int y1) {
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+        int yi = 1;
+        if (dy < 0) {
+            yi = -1;
+            dy = -dy;
+        }
+        int D = 2 * dy - dx;
+        int y = y0;
+
+        for (int x = x0; x < x1; x++) {
+            InputTransporter.getInstance().addPoint(x, y, color);
+            if (D > 0) {
+                y = y + yi;
+                D = D - 2 * dx;
+            }
+            D = D + 2 * dy;
+        }
+    }
+
+    private void plotLineHigh(int x0, int y0, int x1, int y1) {
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+        int xi = 1;
+        if (dx < 0) {
+            xi = -1;
+            dx = -dx;
+        }
+        int D = 2 * dx - dy;
+        int x = x0;
+
+        for (int y = y0; y < y1; y++) {
+            InputTransporter.getInstance().addPoint(x, y, color);
+            if (D > 0)
+                x = x + xi;
+            D = D - 2 * dy;
+            D = D + 2 * dx;
+        }
+    }
+
+    private void plotLine(Point currPoint) {
+        int x0 = lastTouch.x;
+        int y0 = lastTouch.y;
+        int x1 = currPoint.x;
+        int y1 = currPoint.y;
+
+        if (Math.abs(y1 - y0) < Math.abs(x1 - x0)) {
+            if (x0 > x1) {
+                plotLineLow(x1, y1, x0, y0);
+            } else {
+                plotLineLow(x0, y0, x1, y1);
+            }
+        } else {
+            if (y0 > y1) {
+                plotLineHigh(x1, y1, x0, y0);
+            } else {
+                plotLineHigh(x0, y0, x1, y1);
+            }
         }
     }
 
@@ -54,7 +141,7 @@ public class PencilInputTool implements InputTool {
             Log.e("filterX", "maxX not set yet");
             return 0;
         }
-        return (int)((width / maxX) * x);
+        return (int) ((width / maxX) * x);
     }
 
     private int filterY(int y) {
@@ -62,10 +149,14 @@ public class PencilInputTool implements InputTool {
             Log.e("filterY", "maxY not set yet");
             return 0;
         }
-        return (int)((height / maxY) * y);
+        return (int) ((height / maxY) * y);
     }
 
-    public void setColor(int color) { this.color = color; }
+    public void setColor(int color) {
+        this.color = color;
+    }
 
-    public void setThickness(int thickness) { this.thickness = thickness; }
+    public void setThickness(int thickness) {
+        this.thickness = thickness;
+    }
 }
